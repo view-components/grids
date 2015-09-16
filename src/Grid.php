964a3145ce55be09735cc7e35a7f828b9b568785
Vitaliy Stepanenko <mail@vitaliy.in>
2015-09-16 19:14:02 +0300

    GridComposer class added

--- src/Grid.php
+++ src/Grid.php
@@ -4,11 +4,7 @@ namespace Presentation\Grids;
 use Nayjest\Tree\NodeTrait;
 use Presentation\Framework\Base\ComponentInterface;
 use Presentation\Framework\Base\ComponentTrait;
-use Presentation\Framework\Component\Html\Tag;
-use Presentation\Framework\Component\Repeater;
-use Presentation\Framework\Component\Text;
 use Presentation\Framework\Rendering\ViewTrait;
-use Presentation\Grids\Component\Tr;
 
 class Grid implements ComponentInterface
 {
@@ -44,54 +40,8 @@ class Grid implements ComponentInterface
 
     protected function makeComponents()
     {
-        $config = $this->config;
-        $container = $config->getContainer();
-
-        $form = $config->getForm();
-        $form->attachTo($container);
-
-        $table = $config->getTable();
-        $table->attachTo($form);
-
-        $tHead = $config->getTHead();
-        $tHead->attachTo($table);
-
-        $labelTextComponent = new Text;
-        $tHead->addChild(
-            new Tag('tr', null, [new Repeater(
-                $config->getColumns(),
-                [new Tag('th', null, [$labelTextComponent])],
-                function ($repeater, Column $column) use ($labelTextComponent) {
-                    $labelTextComponent->setValue($column->getLabel());
-                }
-            )])
-        );
-
-        $config->getControlPlacingStrategy()->placeControls($config);
-
-        $tBody = $config->getTBody();
-        $tBody->attachTo($table);
-
-        $tFoot = $config->getTFoot();
-        $tFoot->attachTo($table);
-
-        $rowRepeater = $config->getRowRepeater();
-        $rowRepeater->setIterator($config->getDataProvider());
-        $rowRepeater->attachTo($tBody);
-
-        /** @var Tr $row */
-        $row = $config->getTRow();
-        $row->attachTo($rowRepeater);
-
-        $columnRepeater = $config->getColumnRepeater();
-        $columnRepeater->setIterator($config->getColumns());
-        $columnRepeater->attachTo($row);
-
-        $cell = $config->getTCell();
-        $cell->attachTo($columnRepeater);
-        $row->setTCell($cell);
-
-        return [$container];
+        $composer = new GridComposer();
+        return $composer->compose($this->config);
     }
 
     public function applyOperations()
