90b7eaadcdf7ddf45cb31a77818e7d21f108b5f2
v.stepanenko <mail@vitaliy.in>
2016-03-14 14:46:10 +0200

    phpDoc

--- src/Component/TableCaption.php
+++ src/Component/TableCaption.php
@@ -9,6 +9,9 @@ use ViewComponents\ViewComponents\Component\Compound;
 use ViewComponents\ViewComponents\Component\DataView;
 use ViewComponents\ViewComponents\Component\Html\Tag;
 
+/**
+ * This class is a component for rendering grid caption using 'caption' tag.
+ */
 class TableCaption extends Tag implements PartInterface
 {
     use PartTrait {
@@ -20,6 +23,12 @@ class TableCaption extends Tag implements PartInterface
     /** @var DataView  */
     private $text;
 
+    /**
+     * TableCaption constructor.
+     *
+     * @param null|string $text
+     * @param array $attributes caption tag attributes
+     */
     public function __construct($text, $attributes = [])
     {
         parent::__construct('caption', $attributes);
@@ -28,12 +37,21 @@ class TableCaption extends Tag implements PartInterface
         $this->addChild($this->text = new DataView($text));
     }
 
+    /**
+     * This method is overridden for adding caption to beginning
+     * of parent container instead of default appending.
+     *
+     * @param Compound $root
+     * @param bool $prepend
+     */
     public function attachToCompound(Compound $root, $prepend = true)
     {
         $this->attachToCompoundInternal($root, $prepend);
     }
 
     /**
+     * Returns caption text.
+     *
      * @return string
      */
     public function getText()
@@ -42,6 +60,8 @@ class TableCaption extends Tag implements PartInterface
     }
 
     /**
+     * Sets caption text.
+     *
      * @param string $text
      */
     public function setText($text)
