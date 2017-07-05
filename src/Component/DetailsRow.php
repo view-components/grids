882418534ecd914f1ce064a8cb2b5d0586968b42
hexusdev <hexus.dev@gmail.com>
2017-07-05 13:15:53 +0300

    don't include jquery again

--- src/Component/DetailsRow.php
+++ src/Component/DetailsRow.php
@@ -82,9 +82,12 @@ class DetailsRow extends SolidRow implements PartInterface
             );
         }
         $tr->setAttribute('data-row-with-details', 1);
-        $this->getGrid()->children()
-            ->add($this->jquery, 1)
-            ->add($this->getScript());
+       
+        if(!$this->resourceManager->includedResources()->isIncluded($this->jquery->getAttribute('src'))){
+            $this->getGrid()->children()->add($this->jquery, 1);
+        }
+        $this->getGrid()->children()->add($this->getScript());
+        
         // fix zebra styled tables
         $this->parent()->addChild(new DataView('<tr style="display: none"></tr>'));
     }
